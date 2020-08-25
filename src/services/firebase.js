e0070d8ead3894765842b79bc19c85014ad7e9e5
Samuel Antwi <samuelantwi@MacBook-Pro.local>
2020-08-25 13:55:10 +0100

    added firebase key to config file

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,17 +1,8 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import { firebaseConfig } from './config';
 
-var firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: 'cinenight-60e02.firebaseapp.com',
-  databaseURL: 'https://cinenight-60e02.firebaseio.com',
-  projectId: 'cinenight-60e02',
-  storageBucket: 'cinenight-60e02.appspot.com',
-  messagingSenderId: '344827260790',
-  appId: '1:344827260790:web:bf1544d29567eb59071457',
-};
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore();
